c1c29b33c8433e8b11cd93f2884397c3bb7f9dd2
pwappy-com <pwappy.com@gmail.com>
2025-01-16 17:35:08 +0900

    Add files via upload

--- nanimiru-app/main.js
+++ nanimiru-app/main.js
@@ -2,7 +2,7 @@ function 定数() {
     
 }
 
-const serverBase = "https://script.google.com/macros/s/AKfycbwFsQocPUk6ExzwxZkhWEtdoY9RFAllNUq3mVc9QX8AIURqNdUJh3jfABDeVr7AnmprgQ/exec";
+const serverBase = "https://script.google.com/macros/s/AKfycbxqkScqbsamZ0Y0p-7wv3kErXgRBxm8L9DFEDxG6SRqdVfzAsY5DllAATYTxNVn7DGwzQ/exec";
 
 //8B
 //const serverBase = "https://script.google.com/macros/s/AKfycbyAXBQQFrDREpLDNti_7SZ024Jl4Vby-vWQKpeBkZfkm9dTjEDRYHTGDhJqSPD-O_MKwQ/exec";
